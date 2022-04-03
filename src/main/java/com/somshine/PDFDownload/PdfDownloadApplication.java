79b9f44ccac7bc3e6d06462752a592a677a9abd9
Somnath <soma.shinde@gmail.com>
2022-04-03 11:31:05 +0530

    Initial first commit

---
+++ src/main/java/com/somshine/PDFDownload/PdfDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.somshine.PDFDownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somshine/PDFDownload/PdfDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somshine.PDFDownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
